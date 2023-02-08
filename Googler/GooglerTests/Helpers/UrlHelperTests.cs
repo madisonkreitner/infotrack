5fffa50a830a906317cee81d0a0101bd154a1f0b
Madison Kreitner <mkreitner@pfl.com>
2023-02-08 12:13:27 -0500

    Unit tests project set up

---
+++ Googler/GooglerTests/Helpers/UrlHelperTests.cs
@@ -0,0 +1,29 @@
+﻿using Googler.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GooglerTests.Helpers
+{
+    [TestFixture]
+    internal class UrlHelperTests
+    {
+        [Test]
+        [Category("UnitTest")]
+        [TestCase("lkjahsdkfjhasldkfjh","")]
+        // expect to see http or https, this should not work, this can be extended later
+        [TestCase("www.infotrack.com", "")]
+        // we look for a slash after, this should not work, extend this to look for all valid domain suffix
+        [TestCase("https://www.infotrack.com", "www.infotrack.com")]
+        [TestCase("https://www.infotrack.com/blog/integrated-efiling-improve-efficiency-law-firm/&amp;sa=U&amp;ved=2ahUKEwjn-aLUqYb9AhU_TDABHdLCD8QQFnoECAIQAg&amp;usg=AOvVaw0ZF2B9m2o7o_1c55jBQ8Rp", "www.infotrack.com")]
+        [TestCase("http://www.infotrack.com/blog/integrated-efiling-improve-efficiency-law-firm/&amp;sa=U&amp;ved=2ahUKEwjn-aLUqYb9AhU_TDABHdLCD8QQFnoECAIQAg&amp;usg=AOvVaw0ZF2B9m2o7o_1c55jBQ8Rp", "www.infotrack.com")]
+        [TestCase("/url?q=https://www.infotrack.com/blog/integrated-efiling-improve-efficiency-law-firm/&amp;sa=U&amp;ved=2ahUKEwjn-aLUqYb9AhU_TDABHdLCD8QQFnoECAIQAg&amp;usg=AOvVaw0ZF2B9m2o7o_1c55jBQ8Rp", "www.infotrack.com")]
+        [TestCase("/url?q=http://www.infotrack.com/blog/integrated-efiling-improve-efficiency-law-firm/&amp;sa=U&amp;ved=2ahUKEwjn-aLUqYb9AhU_TDABHdLCD8QQFnoECAIQAg&amp;usg=AOvVaw0ZF2B9m2o7o_1c55jBQ8Rp", "www.infotrack.com")]
+        public void GetDomainNameTest(string url, string expectedDomain)
+        {
+            Assert.That(UrlHelper.GetDomainName(url), Is.EqualTo(expectedDomain));
+        }
+    }
+}
---
+++ Googler/GooglerTests/Services/Google/GoogleServiceTests.cs
@@ -0,0 +1,64 @@
+﻿using Googler.Models;
+using Googler.Services.Google;
+using Googler.Services.Html;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GooglerTests.Services.Google
+{
+    [TestFixture]
+    internal class GoogleServiceTests
+    {
+        private GoogleService _service;
+        private Mock<ILogger<GoogleService>> _logger;
+        private Mock<IHttpClientFactory> _httpClientFactory;
+        private Mock<IOptions<HttpClientConfiguration>> _options;
+        private Mock<IHtmlService> _htmlService;
+        private Mock<HttpClient> _httpClient;
+
+        [SetUp]
+        public void Setup()
+        {
+            _logger = new();
+            _httpClientFactory = new();
+            _options = new();
+            _htmlService = new();
+            _httpClient = new();
+            _httpClientFactory.Setup(x => x.CreateClient(It.IsAny<string>())).Returns(_httpClient.Object);
+            HttpClientConfiguration options = new()
+            {
+                BaseUrl = "https://www.example.com"
+            };
+
+            _options.SetupGet(o => o.Value).Returns(options);
+            _service = new(_logger.Object, _httpClientFactory.Object, _options.Object, _htmlService.Object);
+        }
+
+        [Test]
+        [Category("UnitTest")]
+        public void GetSearchResultsTest()
+        {
+            _htmlService.Setup(s => s.GetSearchResultsFromHtml(It.IsAny<string>())).Returns(new List<string>() { "1", "2" });
+            int count = _service.GetSearchResults("123", 123).Result.Count();
+            Assert.That(count, Is.EqualTo(0));
+        }
+
+        [Test]
+        [Category("UnitTest")]
+        public void GetSearchResultsThrowsTest()
+        {
+            _htmlService.Setup(s => s.GetSearchResultsFromHtml(It.IsAny<string>())).Throws(new InvalidOperationException());
+            Assert.ThrowsAsync<InvalidOperationException>(async () => await _service.GetSearchResults("123", 123));
+
+            _htmlService.Setup(s => s.GetHrefFromATag(It.IsAny<string>())).Throws(new InvalidOperationException());
+            Assert.ThrowsAsync<InvalidOperationException>(async () => await _service.GetSearchResults("123", 123));
+        }
+    }
+}
